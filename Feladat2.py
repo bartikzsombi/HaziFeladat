572119dc2153a81977e7a8da3e0715894f213623
bartikzsombi <36511610+bartikzsombi@users.noreply.github.com>
2018-03-04 20:58:35 +0100

    ...

--- Feladat2.py
+++ Feladat2.py
@@ -11,11 +11,32 @@ def fel_1(a):
     else:
         return False
 
+def fel_2(n):
+    db=0
+    i=0
+    while db!=n:
+        if i==2:
+            db+=1
+        elif i%2==1:
+            xdb=0
+            for j in range(1,i+1):
+                if i%j==0:
+                    xdb+=1
+            if xdb==2:
+                db+=1
+        i+=1
+    print(i-1)
+
+def fel_3(x):
+    for i in range(x):
+        if 2**i  >=x:
+            return 2**i
 
 
 
 def main():
     #print(fel_1(8))
-
+    # fel_2(5)
+    # print(fel_3(513))
 
 main()
