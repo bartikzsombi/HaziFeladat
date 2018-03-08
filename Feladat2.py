2835fb9487619c3411a4078f3979c93b9b935eaf
bartikzsombi <36511610+bartikzsombi@users.noreply.github.com>
2018-03-08 23:05:45 +0100

    negyes kész ötös még hibás

--- Feladat2.py
+++ Feladat2.py
@@ -32,11 +32,42 @@ def fel_3(x):
         if 2**i  >=x:
             return 2**i
 
+def fel_4():
+    for i in range(100,1000):
+        lst=[]
+        for j in str(i):
+            lst.append(j)
+        a=lst[0]
+        b=lst[1]
+        c=lst[2]
+        #print(lst)
 
+        if a!=b and a!=c and b!=c:
+            print (i)
+
+def fel_5(n):
+    lst=[]
+    for i in range(1,n+1):
+        db=0
+        for j in range(1,i+1):
+            if i%j==0:
+                db+=1
+        lst.append(db)
+    print(lst)
+    legN =0
+    hanyadik=0
+    for x in lst:
+        hanyadik+=1
+        if lst[x]>legN:
+            legN=lst[x]
+            print(legN)
+            #print(hanyadik)
 
 def main():
-    #print(fel_1(8))
+    # print(fel_1(8))
     # fel_2(5)
     # print(fel_3(513))
+    #fel_4()
+    fel_5(15)
 
 main()
