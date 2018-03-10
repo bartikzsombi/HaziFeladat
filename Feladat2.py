f5ee5d6fa6d89257979510ce0e11f93ee546284d
bartikzsombi <36511610+bartikzsombi@users.noreply.github.com>
2018-03-10 17:08:17 +0100

    9-esig és még a konverzio

--- Feladat2.py
+++ Feladat2.py
@@ -53,21 +53,106 @@ def fel_5(n):
             if i%j==0:
                 db+=1
         lst.append(db)
-    print(lst)
+    #print(lst)
     legN =0
     hanyadik=0
     for x in lst:
         hanyadik+=1
-        if lst[x]>legN:
-            legN=lst[x]
-            print(legN)
-            #print(hanyadik)
+        if x>legN:
+            legN=x
+            #print(legN)
+            print(hanyadik)
+
+def fel_6():
+    a=input("szam1: ")
+    b=input("szam2: ")
+    lst=[]
+    lstA=[]
+    lstB=[]
+    for i in range(len(a)):
+        lstA.append(a[i])
+    for j in range(len(b)):
+        lstB.append(b[j])
+    db=0
+    for x in lstA:
+        for y in lstB:
+            if x==y:
+                lst.append(x)
+                break
+    szamj = [0,1,2,3,4,5,6,7,8,9]
+    for k in szamj:
+        for l in lst:
+            if int(l)==int(k) :
+                db+=1
+                break
+    if db>1:
+        print("eszek a számok rokonok")
+
+def fel_7():
+    a = input("szam1: ")
+    b = input("szam2: ")
+    lst = []
+    lstA = []
+    lstB = []
+    for i in range(len(a)):
+        lstA.append(a[i])
+    for j in range(len(b)):
+        lstB.append(b[j])
+    db = 0
+    for x in lstA:
+        for y in lstB:
+            if x == y:
+                lst.append(x)
+                break
+    szamj = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for k in szamj:
+        for l in lst:
+            if int(l) == int(k):
+                db += 1
+                break
+    print("szam1: {}, szam2:{}".format(lstA,lstB))
+    if db > 0:
+        print("eszek a számok barátok")
+
+def fel_8(n):
+    db=0
+    osszeg=0
+    for i in range(1,n):
+        osszeg+=i
+        db+=1
+        if osszeg>=n:
+            print(db)
+            break
+
+def fel_9():
+    paszuj=1
+    perc=0
+    i=2
+    while paszuj!=300:
+        paszuj+=paszuj/i
+        i+=1
+        perc+=1
+    print(perc)
+
+def konverzio(n,p,x): #(szám,számrendszer,hatvány)
+    if n==0:
+        return 0
+    else:
+        x+=1
+        print((p**x)*(n%10))
+        return konverzio(n//10,p,x) +(p**x)*(n%10)
 
 def main():
     # print(fel_1(8))
     # fel_2(5)
     # print(fel_3(513))
     #fel_4()
-    fel_5(15)
+    #fel_5(15)
+    #fel_6()
+    #fel_7()
+    #fel_8(100)
+    #fel_9()
+    print(konverzio(1101, 2, -1))
+
 
 main()
